f35a7bfb416cade0844d1da06e7a4ba8fc6efe42
Ljazz <elabdrasulov@gmail.com>
2022-09-02 02:52:19 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
+if DEBUG == False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
