6ad680076fd0e17ef47425c6facd6c5e6a5b5201
Ljazz <elabdrasulov@gmail.com>
2022-09-01 16:04:46 +0600

    tests

--- apps/video/check_size.py
+++ apps/video/check_size.py
@@ -3,4 +3,5 @@ from django.core.exceptions import ValidationError
 def file_size(value):
     limit = 50 * 1024 * 1024
     if value.size > limit:
-        raise ValidationError('File too large. Size should not exceed 2 MiB.')
+        raise ValidationError('File too large. Size should not exceed 50 Mb.')
+
